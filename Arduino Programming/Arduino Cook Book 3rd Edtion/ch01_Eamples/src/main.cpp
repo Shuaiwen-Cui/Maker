66c6f2e75e053c9b67bff7143394ccea3b8e6482
Shuaiwen Cui <cuishuaiwen@Shuaiwens-Mac-mini.local>
2023-08-28 18:09:38 +0800

    some updates

--- Arduino Programming/Arduino Cook Book 3rd Edtion/ch01_Eamples/src/main.cpp
+++ Arduino Programming/Arduino Cook Book 3rd Edtion/ch01_Eamples/src/main.cpp
@@ -309,3 +309,391 @@
 //    digitalWrite(LED_BUILTIN, LOW);
 //    delay(blinkDelay/2);  // wait for the other half
 // }
+
+// // example 11
+// /*
+//   blink3 sketch
+//   Demonstrates calling a function with a parameter and returning a value.
+
+//   The LED flashes when the program starts and stops when a switch connected
+//   to digital pin 2 is pressed.
+//   The program prints the number of times that the LED flashes.
+//  */
+
+// const int inputPin = 2;         // input pin for the switch
+
+// int blink3(int period);
+
+// void setup() {
+//   pinMode(LED_BUILTIN, OUTPUT);
+//   pinMode(inputPin, INPUT);
+//   digitalWrite(inputPin,HIGH); // use internal pull-up resistor (Recipe 5.2)
+//   Serial.begin(9600);
+// }
+
+// void loop(){
+//   Serial.println("Press and hold the switch to stop blinking");
+//   int count = blink3(250); // blink the LED 250 ms on and 250 ms off
+//   Serial.print("The number of times the switch blinked was ");
+//   Serial.println(count);
+//   while(digitalRead(inputPin) == LOW)
+//   {
+//     // do nothing until they let go of the button
+//   }
+// }
+
+// // blink an LED using the given delay period
+// // return the number of times the LED flashed
+// int blink3(int period)
+// {
+//   int blinkCount = 0;
+
+//   while(digitalRead(inputPin) == HIGH) // repeat until switch is pressed
+//                                        // (it will go low when pressed)
+//   {
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     delay(period);
+//     digitalWrite(LED_BUILTIN, LOW);
+//     delay(period);
+//     blinkCount = blinkCount + 1; // increment the count
+//   }
+//   // here when inputPin is no longer HIGH (means the switch is pressed)
+//   return blinkCount;  // this value will be returned
+// }
+
+// // example 12
+// /*
+//   swap sketch
+//   demonstrates changing two values using global variables
+//  */
+
+// int x; // x and y are global variables
+// int y;
+
+// void swap();
+
+// void setup() {
+//   Serial.begin(9600);
+// }
+
+// void loop(){
+//   x = random(10); // pick some random numbers
+//   y = random(10);
+
+//   Serial.print("The value of x and y before swapping are: ");
+//   Serial.print(x); Serial.print(","); Serial.println(y);
+//   swap();
+
+//   Serial.print("The value of x and y after swapping are: ");
+//   Serial.print(x); Serial.print(","); Serial.println(y);Serial.println();
+
+//   delay(1000);
+// }
+
+// // swap the two global values
+// void swap()
+// {
+//   int temp;
+//   temp = x;
+//   x = y;
+//   y = temp;
+// }
+
+// // example 13
+// /*
+//   functionReferences sketch
+//   demonstrates returning more than one value by passing references
+//  */
+
+// void swapRef(int &value1, int &value2);
+
+// void setup()
+// {
+//     Serial.begin(9600);
+// }
+
+// void loop()
+// {
+//     int x = random(10); // pick some random numbers
+//     int y = random(10);
+
+//     Serial.print("The value of x and y before swapping are: ");
+//     Serial.print(x);
+//     Serial.print(",");
+//     Serial.println(y);
+//     swapRef(x, y);
+
+//     Serial.print("The value of x and y after swapping are: ");
+//     Serial.print(x);
+//     Serial.print(",");
+//     Serial.println(y);
+//     Serial.println();
+
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     delay(1000);
+//     digitalWrite(LED_BUILTIN, LOW);
+//     delay(1000);
+// }
+
+// // swap the two given values
+// void swapRef(int &value1, int &value2)
+// {
+//     int temp;
+//     temp = value1;
+//     value1 = value2;
+//     value2 = temp;
+// }
+
+// // example 14
+// /*
+//   struct sketch
+//   demonstrates returning more than one value by using a struct
+//  */
+
+// struct Pair
+// {
+//     int a, b;
+// };
+
+// Pair swap(Pair pair);
+
+// void setup()
+// {
+//     Serial.begin(9600);
+// }
+
+// void loop()
+// {
+//     int x = random(10); // pick some random numbers
+//     int y = random(10);
+//     struct Pair mypair = {random(10), random(10)};
+
+//     Serial.print("The value of x and y before swapping are: ");
+//     Serial.print(mypair.a);
+//     Serial.print(",");
+//     Serial.println(mypair.b);
+//     mypair = swap(mypair);
+
+//     Serial.print("The value of x and y after swapping are: ");
+//     Serial.print(mypair.a);
+//     Serial.print(",");
+//     Serial.println(mypair.b);
+//     Serial.println();
+
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     delay(1000);
+//     digitalWrite(LED_BUILTIN, LOW);
+//     delay(1000);
+// }
+
+// // swap the two given values
+// Pair swap(Pair pair)
+// {
+//     int temp;
+//     temp = pair.a;
+//     pair.a = pair.b;
+//     pair.b = temp;
+//     return pair;
+// }
+
+// // example 15
+// /*
+//    ForLoop sketch
+//    demonstrates for loop
+// */
+
+// void setup() {
+//   Serial.begin(9600);
+// }
+
+// void loop(){
+//   Serial.println("for(int i=0; i < 4; i++)");
+//   for(int i=0; i < 4; i++)
+//   {
+//     Serial.println(i);
+//   }
+//   delay(1000);
+// }
+
+// // example 16
+// /*
+//  * break sketch
+//  * Demonstrates the use of the break statement
+//  */
+
+// const int switchPin = 2; // digital input 2
+
+// void blink();
+
+// void setup()
+// {
+//   Serial.begin(9600);
+//   pinMode(LED_BUILTIN, OUTPUT); // enable LED pin as output
+//   pinMode(switchPin, INPUT_PULLUP); // enable button pin as input
+// }
+
+// void loop()
+// {
+//   while(true) // endless loop
+//   {
+//     if(digitalRead(switchPin) == LOW)
+//     {
+//       break; // exit the loop if the switch is pressed
+//     }
+//     blink(); // call a function to turn an LED on and off
+//   }
+// }
+
+// void blink()
+// {
+//   digitalWrite(LED_BUILTIN, HIGH);
+//   delay(100);
+//   digitalWrite(LED_BUILTIN, LOW);
+//   delay(100);
+// }
+
+// // example 17
+// /*
+//  * SwitchCase sketch
+//  * example showing switch statement by switching on chars from the serial port
+//  *
+//  * sending the character 1 blinks the LED once, sending 2 blinks twice
+//  * sending + turns the LED on, sending - turns it off
+//  * any other character prints a message to the Serial Monitor
+//  */
+
+// void blink();
+
+// void setup()
+// {
+//   Serial.begin(9600); // Initialize serial port to send and
+//                       // receive at 9600 baud
+//   pinMode(LED_BUILTIN, OUTPUT);
+// }
+
+// void loop()
+// {
+//   if ( Serial.available()) // Check to see if at least one
+//                            // character is available
+//   {
+//     char ch = Serial.read();
+//     switch(ch)
+//     {
+//     case '1':
+//       blink();
+//       break;
+//     case '2':
+//       blink();
+//       blink();
+//       break;
+//     case '+':
+//       digitalWrite(LED_BUILTIN, HIGH);
+//       break;
+//     case '-':
+//       digitalWrite(LED_BUILTIN, LOW);
+//       break;
+//     case '\N': // newline, safe to ignore
+//       break;
+//     case '\R': // carriage return, safe to ignore
+//       break;
+//     default:
+//       Serial.print(ch);
+//       Serial.println(" was received but not expected");
+//       break;
+//     }
+//   }
+// }
+
+// void blink()
+// {
+//   digitalWrite(LED_BUILTIN, HIGH);
+//   delay(500);
+//   digitalWrite(LED_BUILTIN, LOW);
+//   delay(500);
+// }
+
+// // example 18
+// /*
+//  * RelationalExpressions sketch
+//  * demonstrates comparing values
+//  */
+
+// int i = 1;  // some values to start with
+// int j = 2;
+
+// void setup() {
+//   Serial.begin(9600);
+// }
+
+// void loop(){
+//   Serial.print("i = ");
+//   Serial.print(i);
+//   Serial.print(" and j = ");
+//   Serial.println(j);
+
+//   if(i < j)
+//     Serial.println(" i is less than j");
+//   if(i <= j)
+//     Serial.println(" i is less than or equal to j");
+//   if(i != j)
+//     Serial.println(" i is not equal to j");
+//   if(i == j)
+//     Serial.println(" i is equal to j");
+//   if(i >= j)
+//     Serial.println(" i is greater than or equal to j");
+//   if(i > j)
+//     Serial.println(" i is greater than j");
+
+//   Serial.println();
+//   i = i + 1;
+//   if(i > j + 1)
+//   {
+//     delay(10000);  // long delay after i is no longer close to j
+//   }
+//   else
+//   {
+//     delay(1000);   // short delay
+//   }
+// }
+
+// // example 19
+// /*
+//  * bits sketch
+//  * demonstrates bitwise operators
+//  */
+
+// void setup()
+// {
+//     Serial.begin(9600);
+// }
+
+// void loop()
+// {
+//     Serial.print("3 & 1 equals "); // bitwise And 3 and 1
+//     Serial.print(3 & 1);           // print the result
+//     Serial.print(" decimal, or in binary: ");
+//     Serial.println(3 & 1, BIN); // print the binary representation of the result
+
+//     Serial.print("3 | 1 equals "); // bitwise Or 3 and 1
+//     Serial.print(3 | 1);
+//     Serial.print(" decimal, or in binary: ");
+//     Serial.println(3 | 1, BIN); // print the binary representation of the result
+
+//     Serial.print("3 ^ 1 equals "); // bitwise exclusive or 3 and 1
+//     Serial.print(3 ^ 1);
+//     Serial.print(" decimal, or in binary: ");
+//     Serial.println(3 ^ 1, BIN); // print the binary representation of the result
+
+//     byte byteVal = 1;
+//     int intVal = 1;
+
+//     byteVal = ~byteVal; // do the bitwise negate
+//     intVal = ~intVal;
+
+//     Serial.print("~byteVal (1) equals "); // bitwise negate an 8-bit value
+//     Serial.println(byteVal, BIN);         // print the binary representation of the result
+//     Serial.print("~intVal (1) equals ");  // bitwise negate a 16-bit value
+//     Serial.println(intVal, BIN);          // print the binary representation of the result
+
+//     delay(10000);
+// }
